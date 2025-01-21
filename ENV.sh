2e44f368411c4763e5270857ce84d645e03e8640
ens-LCampbell <lcampbell@ebi.ac.uk>
2025-01-21 20:26:07 +0000

    Update ENV setup redo

--- ENV.sh
+++ ENV.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/bash
+
 cp /homes/lcampbell/bin/duckdb /homes/$USER/bin/duckdb
 NXF_SINGULARITY_NEW_PID_NAMESPACE=false
 export PATH=$PATH:/homes/$USER/bin NXF_SINGULARITY_NEW_PID_NAMESPACE
 
-## Obtain nf-core v3.1.2
+## Obtain nf-core v3.1.2:
+# To be shared on during the breakout!! 
 
-## Don't forget to load nextflow
+## Don't forget to load nextflow:
 # module load nextflow/24.04.3
